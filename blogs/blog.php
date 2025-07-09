8d3c31c36f4ca21688b09305a04a6755d255cfde
Harshvardhan Patil <147519211+harsh6054@users.noreply.github.com>
2025-07-09 17:48:53 +0530

    Add files via upload

---
+++ blogs/blog.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<head><title>Blog Page</title>    
+    <meta charset="utf-8">
+    <title>Hotelier - Hotel HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">  
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style-starter.css">
+
+    
+<style>
+    .btn-banner{
+        background: #333;
+    border-radius: 0px;
+    color: #fff;
+    font-size: 15px;
+    letter-spacing: 1px;
+    padding: 10px 25px;
+    display: inline-block;}
+</style>
+    
+
+    
+</head>
+
+<body>
+<header class="w3l-header-nav">
+	<nav class="navbar navbar-expand-lg navbar-light fill px-lg-0 py-0 px-3">
+		<div class="container">
+			<a class="navbar-brand" href="index.html">
+				<img src="img/logo.png" alt="Your logo" style="height:70px;" /></a>
+
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item @@index__active">
+						<a class="nav-link" href="http://localhost/DrizzleResort/index.php">Home</a>
+					</li>
+					<li class="nav-item @@about__active">
+						<a class="nav-link" href="http://localhost/DrizzleResort/about.php">About</a>
+					</li>
+                    <li class="nav-item @@gallery__active">
+						<a class="nav-link" href="http://localhost/DrizzleResort/gallery/gallery.php">Gallery</a>
+					</li>
+                    <li class="nav-item @@rooms__active">
+						<a class="nav-link" href="http://localhost/DrizzleResort/rooms/room.php">Rooms</a>
+					</li>
+					<li class="nav-item @@services__active">
+						<a class="nav-link" href="http://localhost/DrizzleResort/services.php">Services</a>
+					</li>
+                    <li class="nav-item active">
+						<a class="nav-link" href="http://localhost/DrizzleResort/blogs/blog.php">Blogs</a>
+					</li>
+					<li class="nav-item @@contact__active">
+						<a class="nav-link" href="http://localhost/DrizzleResort/contact.php">Contact</a>
+					</li>
+				</ul>
+				<a href="http://localhost/DrizzleResort/login.php" class="ml-3 book btn btn-secondary btn-style">Logout</a>
+				
+			</div>
+		</div>
+	</nav>
+	<!--//nav-->
+</header>
+
+    
+        <!-- Room Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="text-center wow fadeInUp" style="height: 100%;" data-wow-delay="0.1s">
+                    <h6 class="section-title text-center text-primary text-uppercase">Our Blogs</h6>
+                    <h1 class="mb-5">Explore Our <span class="text-primary text-uppercase">Blogs</span></h1>
+                </div>
+                <div class="row g-4">
+                    <div class="col-lg-4 col-md-6 wow fadeInUp"  data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden"style="height:100%">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/katepoint.jpg" style="height:300px;width: 100%;" alt="">
+                                       </div>
+                            <div class="p-4 mt-2" >
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h5 class="mb-0">Kate Point</h5>
+                                   
+                                </div>
+                               
+                                
+                               
+                                <p class="text-body mb-3">Kate’s point is Located on to the side way of Mahabaleshwar. The huge rock overlooking the Krishna valley is often thronged by tourist.Kate’s point is the most picturesque point of Mahabaleshwar and Panchgani and is the often which used as a cover picture by most books and magazines about Mahabaleshwar.
+ 
+                                    Timing – 6 AM – 6 PM</p>
+                               
+                            </div>
+                            <a href="http://localhost/DrizzleResort/blogs/katepoint.php" class="btn-banner" style="margin-top: 22px;"> View Post </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp"  data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden"style="height:100%">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/A.jpg" style="height:300px;width: 100%;" alt="">
+                                       </div>
+                                     
+                            <div class="p-4 mt-2">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h5 class="mb-0">Arthur's Seat Point, Mahabaleshwar</h5>
+                                   
+                                </div> 
+                                
+                                <p class="text-body mb-3"> Arthur Seat Point is located at a height 1340 m and is surrounded by the deep Savitri gorge on one side and a lush green plateau on the other side.
+                                    Popularly called as Queen of the Points, Arthur’s Seat Point has been named after Sir Arthur Mallet who was the first man to build a House at this Point.  </p>
+                                <br> 
+                            </div>
+                            <a href="http://localhost/DrizzleResort/blogs/arthurs.php" class="btn-banner"> View Post </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden" style="height: 100%;">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/venna-lake.jpg"style="height:300px;width: 100%;" alt="">
+                                       </div>
+                            <div class="p-4 mt-2">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h5 class="mb-0">Venna Lake</h5>
+                                   
+                                </div>
+                               
+                                <p class="text-body mb-3">Venna Lake is a scenic man-made lake in Mahabaleshwar. 
+                                    There is Horse Riding also available. There are number of eateries around the lake Famous for street food like corn, bhelpuri, panipuri.
+                                    The lake is spread over an area of 28acres with a periphery of 7 to 8 km. The best boating experience is in the Evening with the Sunset.
+                                    Timing – 9 AM to 7 PM</p>
+                                
+                            </div>
+                            <a href="http://localhost/DrizzleResort/blogs/vennalake.php" class="btn-banner" style="margin-top: 22px;"> View Post </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden" style="height: 100%;">
+                            <div class="position-relative">
+                                <img style="width: 100%;height:300px;" src="img/P.jpg"  alt="">
+                                       </div>  
+                            <div class="p-4 mt-2">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h5 class="mb-0">Pratapgad
+                                    </h5>
+                                   
+                                </div> 
+                              
+                                <p class="text-body mb-3">A structure of Pratapgad Fort can be divided into regions which are upper and lower. The fort is built atop of time.
+                                    Pratapgad is one of the famous attraction place in Mahabaleshwar where lot of tourists visit every year. It is must see place for History lovers and photographers because of its abundant scenic beauty as well as historical background.
+                                     
+                                    Timing- 8 AM – 6 PM.</p>
+                                <br> <br> 
+                            </div>
+                            <a href="http://localhost/DrizzleResort/blogs/pratapgad.php" class="btn-banner"> View Post </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden"style="height:100%">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/m.jpg" style="height:300px;;width:100%;" alt="">
+                                       </div>
+                            <div class="p-4 mt-2">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h5 class="mb-0"> Mapro Garden Mahabaleshwar</h5>
+                                   
+                                </div>
+                               
+                                <p class="text-body mb-3">Mapro makes fruits-based supplies. There’s choclate factory, restaurant cum cafe, a quant nursery and a children’s play area along with a retail outlet that sells Mapro products.
+                                    The cafe serves scrumptious food made out of organic ingredients.Their speciality is the strawberry with whipped cream served with a smack of deliciousness.</p>
+                                
+                                <br>
+                            </div>
+                            <a href="http://localhost/DrizzleResort/blogs/mapro.php" class="btn-banner" style="margin-top: 24px;"> View Post </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden"style="height:100%">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/Wlingmala.jpg" style="height:300px;width: 100%;" alt="">
+                                       </div>
+                            <div class="p-4 mt-2">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h5 class="mb-0">Lingmala Waterfall</h5>
+                                   
+                                </div>
+                               
+                                <p class="text-body mb-3"> The spectacular sight of Lingmala Waterfall is only enhanced during Mansoon. Situated amid a lush green and vibrant atmosphere, the waterfall attract many trekking enthusiasts and nature lovers due to its gorgeous appeal.
+                                    Lingmala Waterfall constitutes it two waterfalls that confluence at a point, thus forming an oasis. The surrealistic appeal of the place attracts many people.</p>
+                               
+                            </div>
+                            <a href="http://localhost/DrizzleResort/blogs/waterfall.php" class="btn-banner" style="margin-top: 22px;"> View Post </a>
+                        </div>
+                    </div>
+                    
+                    
+                    
+                   
+                </div>
+               
+            </div>
+        </div>
+        
+        
+    </div>
+
+    <section class="w3l-footer-29-main w3l-copyright">
+  <div class="container">
+    <div class="row bottom-copies">
+      <p class="col-lg-8 copy-footer-29">© 2024 Resort. All rights reserved | Powered by<a
+          href="index.html">Drizzle Resort</a></p>
+
+        <div class="col-lg-4 main-social-footer-29">
+            <a href="https://www.facebook.com/profile.php?id=100089083514983" class="facebook"><span class="fa fa-facebook"></span></a>
+            <a href="https://www.instagram.com/drizzle_resort_mahabaleshwar/?igshid=YmMyMTA2M2Y%3D" class="instagram"><span class="fa fa-instagram"></span></a>
+        </div>
+
+    </div>
+  </div>
+<!-- /move top -->
+</section>
+
+
+
+  
+ 
+</body>
+
+</html>
